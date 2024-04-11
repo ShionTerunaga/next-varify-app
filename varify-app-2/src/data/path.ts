7f4c360ca6a3e6e990f012e8881a2a653485b1fa
teru12012000 <sion2769@gmail.com>
2024-04-11 13:28:17 +0900

    feat: add page6

--- varify-app-2/src/data/path.ts
+++ varify-app-2/src/data/path.ts
@@ -4,5 +4,6 @@ export const data: pathType[] = [
   { path: "/page2", pageName: "Go to page2" },
   { path: "/page4", pageName: "Go to page4" },
   { path: "/page5", pageName: "Go to page5" },
+  { path: "/page6", pageName: "Go to page6" },
   { path: "/page2/page3", pageName: "Go to page3" },
 ];
